83a60fe371f31fe1fd590301890db8793d0d5ac5
Marcin Szeniak <marc200333@gmail.com>
2016-07-16 19:28:29 +0200

    Created project

---
+++ NetSettingBinder/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NetSettingBinder")]
+[assembly: AssemblyDescription("Set of tools for binding .Net Settings to Forms controls or any other classes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Marcin Szeniak")]
+[assembly: AssemblyProduct("NetSettingBinder")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c28a3493-8fca-4571-8f9e-27b8420c0720")]
+
+[assembly: AssemblyVersion("1.0.*")]
